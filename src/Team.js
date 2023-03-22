93886084a852aed7c734535d99154152a97e4d1b
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-03-22 05:26:57 -0700

    able to delete teams, reverted change from last merge

--- src/Team.js
+++ src/Team.js
@@ -4,12 +4,13 @@ import React from 'react';
 import TeamMember from './TeamMember';
 import PlaceholderTeamMember from './PlaceholderTeamMember';
 import TeamTypeChart from './TeamTypeChart';
-import { Move } from './lib/pokemon';
+// import { Move } from './lib/pokemon';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Modal from 'react-bootstrap/Modal';
+import Accordion from 'react-bootstrap/Accordion';
 
 class Team extends React.Component{
   constructor(props){
@@ -26,24 +27,28 @@ class Team extends React.Component{
     }
   }
 
+  // handles hiding and showing the type coverage chart
   toggleTypeCoverageModal = () => {
     this.setState({
       showTypeCoverage: !this.state.showTypeCoverage
     })
   }
 
+  // handles hiding and showing the list of teams able to be loaded
   toggleLoadedTeamsModal = () => {
     this.setState({
       showLoadedTeams: !this.state.showLoadedTeams
     })
   }
 
+  // handles hiding and showing the modal for naming a team when saving
   toggleTeamNameModal = () => {
     this.setState({
       showTeamName: !this.state.showTeamName
     })
   }
 
+  // gets all teams from the database > once authentication is implemented this will need to be refactored to only return the specific user's teams
   listTeamsFromDB = async () => {
     try {
       let response = await axios.get(`${process.env.REACT_APP_SERVER}/teams`);
@@ -57,78 +62,13 @@ class Team extends React.Component{
     }
   }
 
+  // gets one specific team from the database
   loadTeam = async (teamId) => {
     console.log(teamId);
     try {
-      let response = 
-        await axios
-        .get(
-          `${process.env.REACT_APP_SERVER}/team?id=${teamId}`
-        )
-        .then(res => {
-          console.log('.then on load team | ', res)
-
-          res.data.pokemon.forEach(async element => {
-            axios
-            .get(`https://pokeapi.co/api/v2/pokemon/${element.name}`)
-
-            .then(res2 => {
-
-              let movesArr = [];
-
-              res2.data.moves.forEach(move => {
-                move.version_group_details.forEach(vgDetail => {
-                  if (vgDetail.version_group.name === 'scarlet-violet') {
-                    movesArr.push(new Move(
-                      move.move.name, 
-                      vgDetail.level_learned_at,
-                      vgDetail.move_learn_method.name,
-                    ))
-                  }
-                })
-              })
-
-              if (movesArr.length === 0) {
-                res2.data.moves.forEach(move => {
-                  move.version_group_details.forEach(vgDetail => {
-                    if(vgDetail.version_group.name === 'sword-shield') {
-                      movesArr.push(new Move(
-                        move.move.name, 
-                        vgDetail.level_learned_at,
-                        vgDetail.move_learn_method.name,
-                      ))
-                    }
-                  })
-                })
-              }
-              element.moves = movesArr;
-              return element;
-            })
-            .then(res2 => {
-              res2.moves.forEach(async move => {
-                try {
-                  let request = {
-                    url: `https://pokeapi.co/api/v2/move/${move.name}`,
-                    method: 'GET'
-                  }
-                  
-                  let res = await axios(request);
-      
-                  move.power = res.data.power;
-                  move.accuracy = res.data.accuracy;
-                  move.pp = res.data.pp;
-                  move.priority = res.data.priority;
-                  move.dmgClass = res.data.damage_class.name;
-                  move.type = res.data.type.name;
-                } catch (err) {
-                  console.log(err)
-                }
-              })
-            })
-          })
-          return res
-        })
-        console.log(response.data);
+      let response = await axios.get(
+        `${process.env.REACT_APP_SERVER}/team?id=${teamId}`
+      )
 
         this.setState({
           team: response.data.pokemon,
@@ -143,21 +83,16 @@ class Team extends React.Component{
     this.toggleLoadedTeamsModal()
   }
 
+  // saves a new team to the database
   saveTeamToDB = (event) => {
     event.preventDefault();
 
-    let savedTeam = this.state.team;
-
-    savedTeam.forEach(element => {
-      element.moves = ['emptied for sake of database space, when team is loaded from database, must make api call to reinstantiate move information.']
-    })
-
     let request = {
       teamName: this.state.teamName,
-      pokemon: savedTeam
+      pokemon: this.state.team
 
     };
-    console.log(request);
+
     axios
       .post(`${process.env.REACT_APP_SERVER}/teams`, request)
       .then(response => {
@@ -166,6 +101,20 @@ class Team extends React.Component{
       .catch(err => {console.log(err)})
 
       this.toggleTeamNameModal();
+  }
+
+  deleteTeamFromDB = async (teamId) => {
+    try {
+      axios
+        .delete(`${process.env.REACT_APP_SERVER}/teams/${teamId}`);
+
+      let response = await axios.get(`${process.env.REACT_APP_SERVER}/teams`);
+      this.setState({
+        loadedTeams: response.data,
+      })      
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   handleInputChange = (event) => {
@@ -221,6 +170,7 @@ class Team extends React.Component{
           </Modal.Body>
         </Modal>
 
+        {/* displays input to name a team before saving */}
         <Modal show={this.state.showTeamName} onHide={this.toggleTeamNameModal} centered>
           <Modal.Header>Save Your Team</Modal.Header>
           <Modal.Body>
@@ -253,15 +203,26 @@ class Team extends React.Component{
           <Modal.Header>Your Teams</Modal.Header>
 
           <Modal.Body>
-            {this.state.loadedTeams.length > 0 ?
-              this.state.loadedTeams.map(element => (
-                <Button onClick={() => this.loadTeam(element.id)}>
-                  {element.teamName}
-                </Button>
-              ))
-            :
-              null
-            }
+            <Accordion>
+              {this.state.loadedTeams.length > 0 ?
+                this.state.loadedTeams.map((element, idx) => (
+                  <Accordion.Item eventKey={idx}>
+                    <Accordion.Header>{element.teamName}</Accordion.Header>
+                    <Accordion.Body>
+                      <Button onClick={() => this.loadTeam(element.id)}>
+                        Load Team
+                      </Button>
+                      <Button onClick={() => this.deleteTeamFromDB(element.id)}>
+                        Delete Team
+                      </Button>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                ))
+              :
+                null
+              }
+            </Accordion>
+
           </Modal.Body>
 
           <Modal.Footer>Footer</Modal.Footer>
@@ -287,3 +248,70 @@ class Team extends React.Component{
 }
 
 export default Team;
+
+// below code not currently used but keeping just in case
+
+// .then(res => {
+//   console.log('.then on load team | ', res)
+
+//   res.data.pokemon.forEach(async element => {
+//     axios
+//     .get(`https://pokeapi.co/api/v2/pokemon/${element.name}`)
+
+//     .then(res2 => {
+
+//       let movesArr = [];
+
+//       res2.data.moves.forEach(move => {
+//         move.version_group_details.forEach(vgDetail => {
+//           if (vgDetail.version_group.name === 'scarlet-violet') {
+//             movesArr.push(new Move(
+//               move.move.name, 
+//               vgDetail.level_learned_at,
+//               vgDetail.move_learn_method.name,
+//             ))
+//           }
+//         })
+//       })
+
+//       if (movesArr.length === 0) {
+//         res2.data.moves.forEach(move => {
+//           move.version_group_details.forEach(vgDetail => {
+//             if(vgDetail.version_group.name === 'sword-shield') {
+//               movesArr.push(new Move(
+//                 move.move.name, 
+//                 vgDetail.level_learned_at,
+//                 vgDetail.move_learn_method.name,
+//               ))
+//             }
+//           })
+//         })
+//       }
+//       element.moves = movesArr;
+//       return element;
+//     })
+//     .then(res2 => {
+//       res2.moves.forEach(async move => {
+//         try {
+//           let request = {
+//             url: `https://pokeapi.co/api/v2/move/${move.name}`,
+//             method: 'GET'
+//           }
+          
+//           let res = await axios(request);
+
+//           move.power = res.data.power;
+//           move.accuracy = res.data.accuracy;
+//           move.pp = res.data.pp;
+//           move.priority = res.data.priority;
+//           move.dmgClass = res.data.damage_class.name;
+//           move.type = res.data.type.name;
+//         } catch (err) {
+//           console.log(err)
+//         }
+//       })
+//     })
+//   })
+//   return res
+// })
+// console.log(response.data);
