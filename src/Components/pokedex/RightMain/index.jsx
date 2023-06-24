2991cd0cd380134504798b170f62ca5cd4855fc3
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-06-23 23:44:37 -0700

    playing with styling

--- src/Components/pokedex/RightMain/index.jsx
+++ src/Components/pokedex/RightMain/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import { ProgressBar } from "react-bootstrap";
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
 import '../../../css/rightCard.css'
 import Learnset from '../../moves/LearnSet';
@@ -15,31 +16,34 @@ function PokedexMainRight (props) {
   return(
     <Container style={{display: 'flex', height: '100%'}} className='rightMain'>
 
-      <Container style={{height: 'inherit', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}} id='pokeInfo'>
+      <Card style={{height: 'inherit', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}} id='pokeInfo' text='white' border='white'>
         <div>
-          <h4>{pokeState.pokemon?.name[0].toUpperCase()+pokeState.pokemon?.name.slice(1)} </h4>
+          <div className='spread'>
+            <h4>{pokeState.pokemon?.name[0].toUpperCase()+pokeState.pokemon?.name.slice(1)} </h4>
+            <p>{`#${pokeState.pokemon?.id.toString().padStart(4, '0')}`}</p>
+          </div>
           <p className='genus'>{pokeState.pokemon?.genus}</p>
-          <p>{`# ${pokeState.pokemon?.id.toString().padStart(4, '0')}`}</p>
+
           <div className='spread'>
             {pokeState.pokemon?.types.map(element => <TypeBadge type={element.type.name} />)}          
           </div>        
         </div>
 
         <div>
-          <div className='centered'>
-            <p className='underline'>Gender Ratio</p>
-            <ProgressBar now={100 - (100 * (pokeState.pokemon?.genderRate / 8))} style={{backgroundColor: 'hotpink', border: '1px solid white'}}/>
-            <p>♂ {100 - (100 * (pokeState.pokemon?.genderRate / 8))} | ♀{100 * pokeState.pokemon?.genderRate / 8}</p>
-          </div>
           <div className='spread'>
             <div className='centered'>
               <p className='underline'>Height</p>
               <p>{pokeState.pokemon?.height.m}m</p>
             </div>
-            <div>
+            <div className='centered'>
               <p className='underline'>Weight</p>
               <p>{pokeState.pokemon?.weight.kg}kg</p>
             </div>
+          </div>
+          <div className='centered'>
+            <p className='underline'>Gender Ratio</p>
+            <ProgressBar now={100 - (100 * (pokeState.pokemon?.genderRate / 8))} style={{backgroundColor: 'hotpink', border: '1px solid white'}}/>
+            <p>♂ {100 - (100 * (pokeState.pokemon?.genderRate / 8))} | ♀{100 * pokeState.pokemon?.genderRate / 8}</p>
           </div>
         </div>
 
@@ -80,7 +84,7 @@ function PokedexMainRight (props) {
           <p className='underline'>Base Exp Yield</p>
           <p>{pokeState.pokemon?.baseExpYield}</p>
         </div>
-      </Container>
+      </Card>
 
       <Container style={{display: 'flex', flexDirection: 'column', justifyContent:'space-between', padding: '0', margin: '0'}} id='pokeMovesAbilities'>
         <Container style={{padding: '0', margin: '0', height: '100%', border: '1px solid white'}}>
@@ -90,7 +94,9 @@ function PokedexMainRight (props) {
           {
             pokeState.pokemon?.name ? 
             <>
+              <br></br>
               <Abilities key={`${pokeState.pokemon?.name}_abilities`}/>
+              <br></br>
               <Learnset key={`${pokeState.pokemon?.name}_moves`}/>            
             </>
             :
